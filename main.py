80e335a6cde49a9d0b9efe4a1b2fc19284793a5e
carlosflorianohlade <carlos.hlade@gmail.com>
2026-08-02 16:15:30 +0200

    Finita ricerca e selezione artista, id artista salvato per le prossime request

--- main.py
+++ main.py
@@ -12,14 +12,30 @@ params = {
     "limit": 10
 }
 
-id_artista = requests.get(
+artista = requests.get(
     "https://api.spotify.com/v1/search",
     headers= headers,
     params= params
 )
 
-data = id_artista.json()
+data = artista.json()
 items = data["artists"]["items"]
 
-for artista in items:
-    print(artista["name"])
+search_result = []
+
+for codice, artista in enumerate(items):
+    print(f"Risultati ricerca:{artista['name']} -- Codice: {codice}")
+    search_result.append(artista['id'])
+
+while True:
+    try:
+        choice = int(input("\nInserisci il ""Codice"" dell'artista: "))
+        if 0 <= choice < len(search_result):
+            break
+        else:
+            print(f"Inserisci un numero tra 0 e {len(search_result) - 1}.")
+    except ValueError:
+        print("Devi inserire un numero.")
+
+id_artista = search_result[choice]
+
